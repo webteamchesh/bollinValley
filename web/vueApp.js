b87df99b07e8323b952500a739ffa7700c263eed
antiloquax <tranter.m@sky.com>
2023-02-02 17:49:30 +0000

    Just pushing recent changes

--- web/vueApp.js
+++ web/vueApp.js
@@ -1,27 +1,95 @@
 var app = new Vue({
   el: "#vueApp",
   data() {
-    let contentType = "Banner";
-    let projectId = "bollinValley";
     return {
       accessToken: "QCpZfwnsgnQsyHHB3ID5isS43cZnthj6YoSPtemxFGtcH15I",
       linkDepth: 5, // leave at 5 if unsure.
-      url: `https://cms-chesheast.cloud.contensis.com/api/delivery/projects/${projectId}/contentTypes/${contentType}/entries/`,
+      rangerPage:
+        "https://www.cheshireeast.gov.uk/leisure,_culture_and_tourism/ranger_service/ranger_events/ranger-event-listings.aspx?event=",
+      url: "https://cms-chesheast.cloud.contensis.com/api/delivery/projects/",
+      events: undefined,
       item: undefined,
-      col: "#FF7F50",
-      contentType: contentType,
+      fromDate: "",
+      toDate: "",
+      dateOptions: {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      },
+      rxDate: /^\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2})?(?:\.\d*)?Z?$/,
     };
   },
   methods: {
-    getBaseUrl: function () {
-      return `${this.url}?accessToken=${this.accessToken}&linkDepth=${this.linkDepth}&pageSize=10`;
+    sortObjDate: function () {
+      return (a, b) => {
+        return a.dateStartEnd.from - b.dateStartEnd.from;
+      };
     },
-    callAxios: function (url) {
+    createDates: function (arr) {
+      return arr.map((e) => {
+        return Object.fromEntries(
+          Object.entries(e).map(([k, v]) => {
+            if (k.toLowerCase().includes("date")) {
+              if (typeof v === "string" && v.match(this.rxDate)) {
+                return [k, new Date(v)];
+              } else if (typeof v === "object") {
+                try {
+                  return [
+                    k,
+                    {
+                      from: new Date(v["from"]),
+                      to: new Date(v["to"]),
+                    },
+                  ];
+                } catch (err) {
+                  return [k, v];
+                }
+              }
+            } else {
+              return [k, v];
+            }
+          })
+        );
+      });
+    },
+    formatDate: function (value) {
+      return value.toLocaleString("en-GB", this.dateOptions);
+    },
+    filterByLocation: function (arr, loc) {
+      return arr.filter((e) => e.specificLocation === loc);
+    },
+    getTime: function (value) {
+      let time = value.toLocaleTimeString([], {
+        hour: "numeric",
+        minute: "2-digit",
+        hour12: true,
+      });
+      if (time === "0:00 pm") {
+        return "12 noon";
+      } else if (time.startsWith("0")) {
+        time = `12${time.slice(1)}`;
+      }
+      return time.replace(" ", "");
+    },
+    baseUrl: function (project, cType) {
+      return `${this.url}${project}/contentTypes/${cType}/entries?accessToken=${this.accessToken}&linkDepth=${this.linkDepth}&pageSize=500`;
+    },
+    callAxios: function (project, cType) {
       axios
-        .get(url)
+        .get(this.baseUrl(project, cType))
         .then((response) => {
-          this.item = response.data.items[0].bannerImage[0];
-          console.log(this.item);
+          if (cType === "banner") {
+            this.item = response.data.items[0].bannerImage[0];
+          } else {
+            this.events = this.filterByLocation(
+              this.createDates(response.data.items),
+              "Bollin Valley"
+            );
+            if (this.events.length > 0) {
+              this.events.sort(this.sortObjDate());
+            }
+          }
         })
         .catch((error) => {
           console.log(error);
@@ -29,6 +97,7 @@ var app = new Vue({
     },
   },
   mounted() {
-    this.callAxios(this.getBaseUrl());
+    this.callAxios("website", "rangerEvents");
+    this.callAxios("bollinValley", "banner");
   },
 });
